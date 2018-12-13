0ab57ef9c95a958ee80245dbff25d70bb45fd547
PJ Frias <pj@SmartLogic.io>
2018-12-13 10:32:08 -0500

    add tests for content context

---
+++ test/koinonia/content/content_test.exs
@@ -0,0 +1,9 @@
+defmodule Koinonia.ContentTest do
+  use Koinonia.DataCase
+
+  alias Koinonia.{Content, Repo}
+
+  test "list_public_prayers/0 returns all public prayers" do
+    # assert true
+  end
+end
