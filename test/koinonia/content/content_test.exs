c6490bc9f4b3daed16095e957d8e111729c29519
PJ Frias <pj@SmartLogic.io>
2018-12-14 12:14:19 -0500

    add tests for Content.build_prayer_request

--- test/koinonia/content/content_test.exs
+++ test/koinonia/content/content_test.exs
@@ -4,13 +4,19 @@ defmodule Koinonia.ContentTest do
   alias Koinonia.{Content, Repo}
   alias Koinonia.Content.PrayerRequest
 
-  setup do
-    Repo.insert(%PrayerRequest{title: "Prayer Request 1 Title", body: "Prayer Request 1 Body"})
-    Repo.insert(%PrayerRequest{title: "Prayer Request 2 Title", body: "Prayer Request 2 Body"})
-    :ok
+  test "build_prayer_request/0 returns a prayer_request changeset" do
+    assert %Ecto.Changeset{data: %PrayerRequest{}} = Content.build_prayer_request()
+  end
+
+  test "build_prayer_request/1 returns a prayer_request with values applied do" do
+    attrs = %{"title" => "PR Title", "body" => "PR Body"}
+    changeset = Content.build_prayer_request(attrs)
+    assert changeset.params == attrs
   end
 
   test "list_prayer_requests/0 returns all public prayers" do
+    Repo.insert(%PrayerRequest{title: "Prayer Request 1 Title", body: "Prayer Request 1 Body"})
+    Repo.insert(%PrayerRequest{title: "Prayer Request 2 Title", body: "Prayer Request 2 Body"})
     [pr1 = %PrayerRequest{}, pr2 = %PrayerRequest{}] = Content.list_prayer_requests()
 
     assert pr1.title == "Prayer Request 1 Title"
