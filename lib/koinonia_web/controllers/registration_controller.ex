a3ab3308420273ce17d04008a88b270747b066ee
PJ Frias <pj@SmartLogic.io>
2018-12-17 11:06:32 -0500

    add create_user fn and added :users table migration

--- lib/koinonia_web/controllers/registration_controller.ex
+++ lib/koinonia_web/controllers/registration_controller.ex
@@ -6,4 +6,17 @@ defmodule KoinoniaWeb.RegistrationController do
     changeset = Account.build_user()
     render(conn, "new.html", changeset: changeset)
   end
+
+  def create(conn, %{"registration" => registration_params}) do
+    case Account.create_user(registration_params) do
+      {:ok, _user} ->
+        conn
+        |> put_flash(:info, "Registration successful")
+        |> redirect(to: Routes.prayer_request_path(conn, :index))
+
+      {:error, changeset} ->
+        conn
+        |> render(:new, changeset: changeset)
+    end
+  end
 end
